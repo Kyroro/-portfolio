f61848bd8e7f33aa7ee11b277eb90abd8d3bf6ec
Kyroro <53153016+Kyroro@users.noreply.github.com>
2019-07-21 23:25:51 +0300

    index

---
+++ lending/script.js
@@ -0,0 +1,11 @@
+// var inrutName =document.getElementById(input-item1);
+// function validate(){
+//     if(!input-item1.value);
+//     {
+//         input-item1.style.border ="2px solid red";
+
+//     }
+
+
+
+// }
